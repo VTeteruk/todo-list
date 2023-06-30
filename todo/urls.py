2afc0df2f8f0814268a459d40df0090957889d41
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-30 13:34:42 +0300

    Implement tag functional

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 
-from todo.views import IndexView, TaskListView, TaskCreateView, TaskStatusUpdateView, TaskUpdateView, TaskDeleteView
+from todo.views import (
+    IndexView,
+    TaskListView,
+    TaskCreateView,
+    TaskStatusUpdateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
@@ -8,7 +19,11 @@ urlpatterns = [
     path("tasks/new/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:task_id>/status-update/", TaskStatusUpdateView.as_view(), name='task-status-update'),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete")
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tags", TagListView.as_view(), name="tags"),
+    path("tags/new/", TagCreateView.as_view(), name="tag-create"),
+    path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 class IndexView(generic.TemplateView):
@@ -41,3 +41,28 @@ class TaskDeleteView(generic.DeleteView):
     model = Task
     template_name = "todo/task_delete.html"
     success_url = reverse_lazy("todo:tasks")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "todo/tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tags")
+    template_name = "todo/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    template_name = "todo/tag_form.html"
+    success_url = reverse_lazy("todo:tags")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tags")
+    template_name = "todo/tag_delete.html"
