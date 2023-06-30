79f47f60f3055299593ad263cf69a4c273772666
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-30 12:05:28 +0300

    Fix/Add auto now to created date & make tags unrequired

--- todo/models.py
+++ todo/models.py
@@ -7,10 +7,12 @@ class Tag(models.Model):
 
 class Task(models.Model):
     content = models.TextField()
-    datetime = models.DateTimeField()
+    datetime = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     is_done = models.BooleanField()
-    tags = models.ManyToManyField(Tag, related_name="tasks")
+    tags = models.ManyToManyField(
+        Tag, related_name="tasks", null=True, blank=True
+    )
 
     class Meta:
         ordering = ("is_done", "datetime")
