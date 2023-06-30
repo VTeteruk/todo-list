7d07764c78f9f8b62bc51f3b8c8c002dff06c0c7
Vlad Tetruk <vladteteruk01@gmail.com>
2023-06-30 10:19:24 +0300

    Implement models: Tag & Task / make migrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField()
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks")
